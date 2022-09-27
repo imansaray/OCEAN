0cf463793d89078e65b49fe855ec346cff78dbd8
John Vinson <jtv3@uw.edu>
2022-09-27 14:33:21 -0400

    WIP

--- XPS/buildAtomCenter.f90
+++ XPS/buildAtomCenter.f90
@@ -8,12 +8,13 @@ program buildAtomCenter
   integer :: natoms
   character(len=2), allocatable :: elnames(:)
   integer, allocatable :: elnumbers(:), zeelist(:)
-  real(DP) :: kappa(4)
+  real(DP) :: kappa(3), avecs(3,3), xbohr(3), coord(3)
   real(DP), allocatable :: xred(:,:), angular(:,:)
+  complex(DP), allocatable ::  full_grid(:,:)
   
   character(len=10) :: spcpnt
 
-  integer : i, n, iatom, zee, j, k, nUniqueZ, ierr
+  integer : i, n, iatom, zee, j, k, nUniqueZ, ierr, nang, nr
 
   ierr = 0
 
@@ -25,6 +26,10 @@ program buildAtomCenter
     read( elnames(i), xred(:,i) )
     get_Atom_number( elnumbers(i), elnames(i) )
   enddo
+  close(99)
+
+  open( unit=99, file='avecsinbohr.ipt', form='formatted', status='old')
+  read(99,*) avecs(:,:)
   close(99)
 
   nUniqueZ = 0
@@ -42,9 +47,9 @@ program buildAtomCenter
   close(99)
   write( spcpnt, '(A,I0') 'specpnt.', i
   open(unit=99, file=spcpnt, form='formatted', status='old' )
-  read( 99, * ) n
-  allocate( angular(4,n) )
-  do i= 1, n
+  read( 99, * ) nang
+  allocate( angular(4,nang) )
+  do i= 1, nang
     read(99,*) angular(:,i)
   enddo
   close(99)
@@ -56,6 +61,7 @@ program buildAtomCenter
 
   call screen_opf_loadAll( ierr, zeelist(1:nUniqueZ) )
 
+  !TODO, only deallocate/re-allocate if grid different sizes
   itarg = 1
   do iatom = 1, natoms
     call screen_opf_getNrad( elnumbers(iatom), nr, ierr, itarg )
@@ -66,9 +72,23 @@ program buildAtomCenter
     if( ierr .ne. 0 ) goto 111
 
     ! allocate angular x radial grid
+    allocate( full_grid( nang, nr ) )
+
     ! step through and fill in X = R + Tau
     ! Then for each can calculate exp i Kappa (r+Tau)
-    ! just calculate directly, 
+    xbohr(:) = matmul( avecs, xred(:,iatom) )
+    !
+    do ir = 1, nr
+      do iang = 1, nang
+        coord( : ) = xbohr(:) + rad(ir) * angular(1:3, iang )
+        full_grid(iang,ir) = exp( cmplx( 0.0_DP, dot_product( kappa, coord ), DP ) )
+      enddo
+    enddo
+
+    
+    ! Now for each n, l, m of the projectors
+    
+    
 
   enddo
 
